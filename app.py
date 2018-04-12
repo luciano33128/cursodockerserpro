3f787c0229bfeba7178d7a0c09860fe277022377
luciano33128 <luciano.alves@serpro.gov.br>
2018-04-12 17:05:04 -0300

    Aula 3 - Exercicio 2

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
+#from flask import Flask, request
+#from logging.handlers import RotatingFileHandler
+
+#from flask.ext.redis import FlaskRedis
+
+import logging
 from flask import Flask, request
 from logging.handlers import RotatingFileHandler
 
 from flask.ext.redis import FlaskRedis
 
-import logging
-import datetime
+#import datetime
 
 #Create the App
 app = Flask(__name__)
